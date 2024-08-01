8bdc82968f4ae5aa0ab6ff51bd37aba8fedf62da
Heitor G. Nunes <heitorgouveianunes@gmail.com>
2024-08-01 13:56:44 -0300

    Commit aula operadores

--- anatomia-classes-java/src/edu/heitor/segundasemana/Operadores.java
+++ anatomia-classes-java/src/edu/heitor/segundasemana/Operadores.java
@@ -2,26 +2,30 @@ package edu.heitor.segundasemana;
 
 public class Operadores {
 
+    
+
+
     public static void main(String[] args) {
-        String concatenacao ="?";
 
-        concatenacao = 1+1+1+"1";
-
-            System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+1;
-
-            System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-
-            System.out.println(concatenacao);
-
-        concatenacao = "1"+1+1+1;
-
-            System.out.println(concatenacao);
-
-        concatenacao = "1"+(1+1+1);
+        int numero1 = 1;
+        int numero2 = 2;
+        
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+        
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+        
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+        
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+        
+        if(numero1 != numero2)
+            System.out.print("Numero 1 é diferente de numero 2");
     }
     
 }
+
+
